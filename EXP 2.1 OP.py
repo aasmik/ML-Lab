d46530789576701fa46574f235057ea18f30e4ea
aasmik <aasmikudhabaksh@gmail.com>
2025-12-31 15:25:51 +0530

    Add files via upload

---
+++ EXP 2.1 OP.py
@@ -0,0 +1,22 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================== RESTART: D:/ML Aasmi/exp 2.1.py ======================
+STUDENT REPORT
+----------------------------------------
+Student ID : S1
+Name       : Ravi
+Marks      : 85
+Grade      : A
+----------------------------------------
+Student ID : S2
+Name       : Meena
+Marks      : 62
+Grade      : B
+----------------------------------------
+Student ID : S3
+Name       : Kiran
+Marks      : 40
+Grade      : C
+----------------------------------------
+>>> 
