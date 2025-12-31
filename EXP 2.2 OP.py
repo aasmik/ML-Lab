43c7666245ce39b0edc74516dc4e1abb6d832fef
aasmik <aasmikudhabaksh@gmail.com>
2025-12-31 15:26:23 +0530

    Add files via upload

---
+++ EXP 2.2 OP.py
@@ -0,0 +1,18 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: D:/ML Aasmi/exp 2.2 basics.py ===================
+LIBRARY ACTIVITY REPORT
+
+B101 | Anu | Borrowed: 2 | Returned: 1
+B102 | Bala | Borrowed: 0 | Returned: 1
+B103 | Chitra | Borrowed: 1 | Returned: 0
+
+BOOK NOT RETURNED REPORT
+B101 has books not returned
+B103 has books not returned
+
+DAILY LIBRARY STATISTICS
+2025-03-10: 4 activities
+2025-03-11: 1 activities
+>>> 
