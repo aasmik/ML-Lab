ec93c3bf38587f8ab88d201a1a8a9351edb292da
aasmik <aasmikudhabaksh@gmail.com>
2026-01-06 23:02:52 +0530

    Update EXP 2.2 OP.py

--- EXP 2.2 OP.py
+++ EXP 2.2 OP.py
@@ -1,18 +1,11 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-=================== RESTART: D:/ML Aasmi/exp 2.2 basics.py ===================
-LIBRARY ACTIVITY REPORT
+S101 | Asha | Logins: 2 | Submissions: 0
+S102 | Ravi | Logins: 0 | Submissions: 1
+S103 | Meena | Logins: 1 | Submissions: 0
 
-B101 | Anu | Borrowed: 2 | Returned: 1
-B102 | Bala | Borrowed: 0 | Returned: 1
-B103 | Chitra | Borrowed: 1 | Returned: 0
+ABNORMAL BEHAVIOR REPORT
+S101 has multiple logins without logout
+S103 has multiple logins without logout
 
-BOOK NOT RETURNED REPORT
-B101 has books not returned
-B103 has books not returned
-
-DAILY LIBRARY STATISTICS
+DAILY ACTIVITY STATISTICS
 2025-03-10: 4 activities
 2025-03-11: 1 activities
->>> 
